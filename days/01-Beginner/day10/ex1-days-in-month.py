ce240cc5e689886e3038ecefcf0dfb0fbf4d1875
Mr. Erlison <sisal.cordame_0a@icloud.com>
2022-12-23 10:31:50 -0300

    Add day 10

---
+++ days/01-Beginner/day10/ex1-days-in-month.py
@@ -0,0 +1,23 @@
+def is_leap(year):
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+        return True
+
+    return False
+
+def days_in_month(year, month):     
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
+    if month == 2 and is_leap(year):
+        return 29
+
+    return month_days[month - 1]
+
+def main():
+    year = int(input("Enter a year: "))
+    month = int(input("Enter a month: "))
+    if (month < 1 or month > 12) or year < 0:
+        print("Invalid value!")
+        exit(1)
+
+    print(days_in_month(year, month))
+
+main()
---
+++ days/01-Beginner/day10/project-calc.py
@@ -0,0 +1,46 @@
+
+def add(n1, n2):
+  """ Add two number and return the result. """
+  return n1 + n2
+
+def subtract(n1, n2):
+  """ Subtract two number and return the result. """
+  return n1 - n2
+
+def multiply(n1, n2):
+  """ Multiply two number and return the result. """
+  return n1 * n2
+
+def divide(n1, n2):
+  """ Divide two number and return the result. """
+  return n1 / n2
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+def calculator():
+  number_1 = float(input("What's the first number?: "))
+  symbol = [x for x in operations]
+
+  while True:
+    operation_symbol = input(f"Pick an operation: {' '.join(symbol)} ")
+    
+    if operation_symbol not in ["+", "-", "*", "/"]:
+      print("invalid operation!")
+      continue
+
+    number_2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(number_1, number_2)
+    print(f"{number_1} {operation_symbol} {number_2} = {answer}")
+    
+    if input(f"Type 'c' to continue with {answer}, or type 'q' to quit: ") == 'q':
+      break
+    
+    number_1 = answer
+
+calculator()
