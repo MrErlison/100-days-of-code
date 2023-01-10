4a19ecb7aca92bcab9b6234afeca2361e9c15808
Mr. Erlison <sisal.cordame_0a@icloud.com>
2023-01-10 17:34:27 -0300

    Updated random color

--- days/day18/ex2-rand-walk.py
+++ days/day18/ex2-rand-walk.py
@@ -5,7 +5,14 @@ which is very interesting as each run will give different output.
 import turtle as t
 import random
 
-COLOURS = ["blue", "green", "yellow", "black", "purple", "brown", "red", "pink"]
+def random_color():
+    """
+    Generates a random RGB color.
+    """
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r,g,b)
 
 def walk(size, total):
     """
@@ -13,10 +20,11 @@ def walk(size, total):
     draw lines, with the given `size` and `total` number of lines.
     """
     obj = t.Turtle()
+    t.colormode(255)
 
     directions = [d for d in range(0, 360, 45)]
     for _ in range(total):
-        obj.color(random.choice(COLOURS))
+        obj.color(random_color())
         obj.forward(size)
         obj.setheading(random.choice(directions))
 
